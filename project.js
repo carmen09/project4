5ea2d9f233fbf474b5cf0b92e30155a54721d6e3
carmen09 <44234186+carmen09@users.noreply.github.com>
2021-04-27 18:17:26 -0400

    ppp

--- project.js
+++ project.js
@@ -2,6 +2,7 @@
  document.onkeydown=function(event){
     var au = document.getElementById("myAudio");
           var e = event || window.event || arguments.callee.caller.arguments[0];
+          
           if(e && e.keyCode==65){ // 按 A 
               //要做的事情
               setcolor();
